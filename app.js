beb655a36e9cced97f0bb686468f344e6beae775
Mitchel Lang <mlang@Mitchels-MBP.home>
2017-07-23 12:51:50 -0400

    Added a comment

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ convert.cToF = function(celsius) {
   return celsius * 9 / 5 + 32;
 }
 
+#from F to C
 convert.fToC = function(fahrenheit) {
   if(!Number.isInteger(fahrenheit)) return undefined;
   return (fahrenheit - 32) * 5 / 9;
