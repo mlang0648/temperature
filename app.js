735066fa202bacf0f177db8ed70d8cda59843f19
Mitchel Lang <mlang@Mitchels-MBP.home>
2017-07-23 13:21:19 -0400

    Change comment from # to //

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let convert = {};
 
-#from C to F
+//from C to F
 convert.cToF = function(celsius) {
   if(!Number.isInteger(celsius)) return undefined;
   return celsius * 9 / 5 + 32;
 }
 
-#from F to C
+//from F to C
 convert.fToC = function(fahrenheit) {
   if(!Number.isInteger(fahrenheit)) return undefined;
   return (fahrenheit - 32) * 5 / 9;
