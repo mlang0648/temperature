49ab030cfab1ff102d891cab579cfef985b21e1e
Mitchel Lang <mlang@Mitchels-MBP.home>
2017-07-24 20:24:15 -0400

    initial commits for testing a second module with a second test harness

---
+++ speak.js
@@ -0,0 +1,12 @@
+let speak = {};
+
+
+speak.word = function() {
+    return "word";
+}
+
+speak.sentence = function() {
+    return "two words";
+}
+
+module.exports = speak;
---
+++ test/test2.js
@@ -0,0 +1,15 @@
+let speak = require('../speak.js')
+let assert = require('assert');
+
+// Create a group of tests about Temperatures
+describe('Say Something', function() {
+  // Within our TemperatureConversion group, Create a group of tests for conversions
+  describe('word', function() {
+    it('should return a word', function() {
+      assert.equal("word", speak.word());
+    });
+    it('should return a sentence', function() {
+      assert.equal("two words", speak.sentence());
+    });
+  });
+});
