96c1b5c2d5b800a942ad4e00265a652cd60a2524
Mitchel Lang <mlang@Mitchels-MBP.home>
2017-07-29 20:48:15 -0400

    fixed it

--- test/test2.js
+++ test/test2.js
@@ -6,7 +6,7 @@ describe('Say Something', function() {
   // Within our TemperatureConversion group, Create a group of tests for conversions
   describe('word', function() {
     it('should return a word', function() {
-      assert.equal("wor", speak.word());
+      assert.equal("word", speak.word());
     });
     it('should return a sentence', function() {
       assert.equal("two words", speak.sentence());
